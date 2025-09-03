60e2f97a685ca64fa933b020771432bfc052acda
deniz2104 <sined2101@gmail.com>
2025-09-03 12:35:05 +0300

    add method to clasiffy it into theme,plugin,etc and updated config file, need to separe them into pro and free

--- config.py
+++ config.py
@@ -8,6 +8,8 @@ CLIENT_ID = os.environ.get("HELPSCOUT_CLIENT_ID")
 CLIENT_SECRET = os.environ.get("HELPSCOUT_CLIENT_SECRET")
 
 MAILBOX_ID = 21530
-PLUGINS_TAGS = ["feedzy","hyve","lightstart","menu-icons","mpg","multiple pages generator","optimole","orbit fox","otter","ppom","redirection-cf7","redirection for cf7","revive old post","revive social","sparks","super-page-cache","super page cache pro","templates-cloud","visualizer","wpcf7","wp full pay","wplandingkit","wp landing kit","wp-media-library","masteriyo"]
+PLUGINS_TAGS = ["feedzy","hyve","lightstart","menu-icons","mpg","multiple pages generator","optimole","orbit fox","otter","ppom","redirection-cf7","redirection for cf7","revive old post","revive-old-post","revive social","sparks","super-page-cache","super page cache pro","templates-cloud","visualizer","wpcf7","wp full pay","wplandingkit","wp landing kit","wp-media-library","masteriyo"]
 THEMES_TAGS = ["neve","hestia","fse","jaxon","raft","zelle","fork","zerif","riverbank","church-fse"]
-WORDPRESS_TICKETS_TAGS =["wporg"]
+WORDPRESS_TICKETS_TAGS = "wporg"
+SHOP_TAGS = ["shop-isle-pro","shopisle-pro"]
+PAYMENT = "optionistics"
--- manage_tags.py
+++ manage_tags.py
@@ -19,3 +19,56 @@ class ManageTags:
             all_tags.extend(tag.get('name', '') for tag in tags_data)
             page += 1
         return all_tags
+
+    def categorize_helpscout_tags(self):
+        all_tags = self.retrieve_all_tags()
+        
+        config_categories = {
+            'plugins_tags': config.PLUGINS_TAGS,
+            'themes_tags': config.THEMES_TAGS,
+            'shop_tickets_tags': config.SHOP_TAGS
+        }
+        
+        results = {
+            'plugins_tags': [],
+            'themes_tags': [],
+            'wordpress_tickets_tags': [config.WORDPRESS_TICKETS_TAGS],
+            'shop_tickets_tags': [],
+            'payment': [config.PAYMENT]
+        }
+
+        def tags_match(helpscout_tag, config_tag):
+            hs_tag = helpscout_tag.lower()
+            cfg_tag = config_tag.lower()
+            
+            if (cfg_tag in hs_tag or hs_tag in cfg_tag) or (hs_tag == cfg_tag):
+                return True
+            
+            return None
+        
+        for hs_tag in all_tags:
+            categorized = False
+            
+            for category, config_tags in config_categories.items():
+                for config_tag in config_tags:
+                    match_type = tags_match(hs_tag, config_tag)
+
+                    if match_type:
+                        results[category].append(hs_tag)
+                        categorized = True
+                        break
+                
+                if categorized:
+                    break
+        
+        return results
+
+
+def main():
+    tag_manager = ManageTags(config.CLIENT_ID, config.CLIENT_SECRET)
+    categorized_tags = tag_manager.categorize_helpscout_tags()
+    pprint(categorized_tags)
+
+
+if __name__ == "__main__":
+    main()
