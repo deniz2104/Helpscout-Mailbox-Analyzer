d42d61fdd128e69b94a8280f02abd38c39c535ae
deniz2104 <sined2101@gmail.com>
2025-09-03 11:42:32 +0300

    added relevant tags in config file

--- config.py
+++ config.py
@@ -8,3 +8,6 @@ CLIENT_ID = os.environ.get("HELPSCOUT_CLIENT_ID")
 CLIENT_SECRET = os.environ.get("HELPSCOUT_CLIENT_SECRET")
 
 MAILBOX_ID = 21530
+PLUGINS_TAGS = ["feedzy","hyve","lightstart","menu-icons","mpg","multiple pages generator","optimole","orbit fox","otter","ppom","redirection-cf7","redirection for cf7","revive old post","revive social","sparks","super-page-cache","super page cache pro","templates-cloud","visualizer","wpcf7","wp full pay","wplandingkit","wp landing kit","wp-media-library","masteriyo"]
+THEMES_TAGS = ["neve","hestia","fse","jaxon","raft","zelle","fork","zerif","riverbank","church-fse"]
+WORDPRESS_TICKETS_TAGS =["wporg"]
