784ffaab71ebde6a29820f6e9dfe8398ab98f1d3
deniz2104 <sined2101@gmail.com>
2025-09-04 14:27:10 +0300

    added method to export ids in a csv in an optimal way, storin them in a list and after writing them, 40 seconds from 4 minutes

--- helpscout_mailbox.py
+++ helpscout_mailbox.py
@@ -30,16 +30,16 @@ class HelpScoutMailboxConversations:
         
         conv_date = self._convert_creation_date(creation_date)
         return start_date <= conv_date <= end_date
-    
-    def export_to_csv(self, conversations : dict, file_path: str,start_date,end_date, write_header: bool = False) -> None:
+
+    def export_list_to_csv(self, conversation_ids: list, file_path: str, write_header: bool = False) -> None:
         mode = 'w' if write_header else 'a'
         with open(file_path, mode, newline='', encoding='utf-8') as file:
             writer = csv.writer(file)
             if write_header:
-                writer.writerow(['Conversation ID', 'Creation Date'])
-            for conversation_id, creation_date in conversations.items():
-                if self._is_in_specified_date_range(creation_date, start_date, end_date):
-                    writer.writerow([conversation_id, self._convert_creation_date(creation_date)])
+                writer.writerow(['Conversation ID'])
+            
+            for conv_id in conversation_ids:
+                writer.writerow([conv_id])
 
     def analyze_last_month_conversations(self):
         start_date, end_date = get_last_month_dates()
@@ -61,7 +61,6 @@ class HelpScoutMailboxConversations:
             if first_conversation_date:
                 first_date = self._convert_creation_date(first_conversation_date)
                 if first_date >= end_date:
-                    print(f"Page {page}: Skipping - conversations too new (newest: {first_date} > {end_date})")
                     page += 1
                     continue
 
@@ -69,16 +68,10 @@ class HelpScoutMailboxConversations:
             if last_conversation_date:
                 last_date = self._convert_creation_date(last_conversation_date)
                 if last_date <= start_date:
-                    print(f"Page {page}: Stopping - conversations too old (oldest: {last_date} < {start_date})")
                     break
+            list_of_ids = [conv['id'] for conv in conversations]
+            self.export_list_to_csv(list_of_ids, "filtered_conversations.csv", write_header=first_page)
 
-            new_conversation = {}
-            for conv in conversations:
-                conv_id = conv['id']
-                creation_date = self._get_creation_date(conv_id)
-                new_conversation[conv_id] = creation_date
-
-            self.export_to_csv(new_conversation,"filtered_conversations.csv", start_date, end_date, write_header=first_page)
             first_page = False
             page += 1
 
