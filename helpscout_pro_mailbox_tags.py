1f576a57b1bf511f78af1eb49178bfade265048b
deniz2104 <sined2101@gmail.com>
2025-09-05 10:12:03 +0300

    missed to add the file in the other commit

---
+++ helpscout_pro_mailbox_tags.py
@@ -0,0 +1,19 @@
+from mailbox_base import MailboxBase
+from config import MAILBOX_PRO_ID,CLIENT_ID, CLIENT_SECRET
+class HelpscoutProMailboxTags(MailboxBase):
+    @property
+    def mailbox_id(self) -> int:
+        return MAILBOX_PRO_ID
+
+    @property
+    def csv_filename(self) -> str:
+        return "filtered_pro_conversations_tags.csv"
+    
+
+def main():
+    helpscout_pro_mailbox = HelpscoutProMailboxTags(CLIENT_ID, CLIENT_SECRET)
+    helpscout_pro_mailbox.analyze_last_month_tags()
+
+if __name__ == "__main__":
+    main()
+
